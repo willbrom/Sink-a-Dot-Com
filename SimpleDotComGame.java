655e6f073e72c86b8bd4c81a5adb79e4f2868f68
willbrom <alisajid552@yahoo.com>
2018-03-27 09:38:49 +0300

    Add simple game class

---
+++ SimpleDotComGame.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SimpleDotComGame {
+  public static void main(String[] args) {
+    int numOfGuesses = 0;
+    SimpleDotCom dot = new SimpleDotCom();
+
+    int[] locationsCells = new int[3];
+
+    // compute random number between 0 and 4
+    int rand = ((int) (Math.random() * 5)) + 1;
+
+    locationsCells[0] = rand;
+    locationsCells[1] = ++rand;
+    locationsCells[2] = ++rand;
+    dot.setLocationCells(locationsCells);
+
+    boolean isAlive = true;
+    while (isAlive) {
+      Scanner sc = new Scanner(System.in);
+      String usersGuess = sc.next();
+      String result = "Miss";
+      result = dot.checkYourself(usersGuess);
+      numOfGuesses++;
+
+      if (result.equals("Kill")) {
+        isAlive = false;
+        System.out.println("It took you " + numOfGuesses + " tries to sink the DotCom!");
+      }
+    }
+  }
+}
