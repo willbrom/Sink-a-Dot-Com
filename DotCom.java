6935a95feb9d83718e3665ed73ae1118de2149ae
willbrom <alisajid552@yahoo.com>
2018-03-31 21:59:04 +0300

    Update to a better and optimized version on SimpleDotCom (Solved bug)

--- DotCom.java
+++ DotCom.java
@@ -1,11 +1,27 @@
-class DotCom {
-	private String name;
+import java.util.ArrayList;
 
-	public void setName(String name) {
-		this.name = name;
+public class DotCom {
+	private ArrayList<String> locationCells;
+
+	public void setLocationCells(ArrayList<String> loc) {
+		this.locationCells = loc;
 	}
 
-	public String getName() {
-		return name;
+	public String checkYourself(String userInput) {
+		String result = "Miss";
+
+		int index = locationCells.indexOf(userInput);
+
+		if (index >= 0) {
+			locationCells.remove(userInput);
+
+			if (locationCells.isEmpty()) {
+				result = "Kill";
+			} else {
+				result = "Hit";
+			}
+		}
+		
+		return result;
 	}
 }
